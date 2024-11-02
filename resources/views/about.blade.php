bc5cd7bc2d1e5f48c4f9e69cdc0361526808c80b
Mohammed <mohayder9199@gmail.com>
2024-11-02 12:49:43 +0300

    good job epi6

--- resources/views/about.blade.php
+++
@@ -1,7 +0,0 @@
-<x-layout>
-   <x-slot:title>
-    About.
-   </x-slot:title>
-
-   This is about page
-</x-layout> 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,5 @@
         Home.
        </x-slot:title>
    This is home page
-   <br>
-   <br><strong>Job titles</strong>
-   @foreach ($jobs as $job)
- 
-      <li>{{$job['title']}}</li>
-   @endforeach
+   
 </x-layout>
